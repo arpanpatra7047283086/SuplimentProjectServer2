2af05f1169cbd1b0d81d0914f83d2a7537004d7d
DONCHAN <arpanpatra800188500@gmail.com>
2026-02-06 22:30:49 +0530

    Update backend User interface1.1...

--- Backend/backend/config/settings.py
+++ Backend/backend/config/settings.py
@@ -122,7 +122,7 @@ SIMPLE_JWT = {
 
 CORS_ALLOWED_ORIGINS = os.environ.get(
     "CORS_ALLOWED_ORIGINS",
-    "http://localhost:3000,http://127.0.0.1:3000"
+    "http://localhost:3000,http://192.168.1.102:3000,https://supliment-project.vercel.app"
 ).split(",")
 
 CORS_ALLOW_CREDENTIALS = True
